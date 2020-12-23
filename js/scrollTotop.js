0ed1670f7fb40e27c08f5208a7e9ba258edd56a9
Roshadgu <roshadgu@gmail.com>
2020-12-23 16:20:01 -0500

    Initial Commit

---
+++ js/scrollTotop.js
@@ -0,0 +1,11 @@
+
+const btnScrollToTop = document.getElementById('btnScrollToTop');
+
+btnScrollToTop.addEventListener("click", function()
+{
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+});
